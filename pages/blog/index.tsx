dc7d5731348cec5bdc9d7372b7c91c65f454bebc
Nelo Puchades <correo@nelo.dev>
2022-03-14 21:44:24 +0100

    Using getStaticProps to get posts list from CMS and from local storage

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -32,9 +32,28 @@ Blog.defaultProps = {
   posts: [],
 }
 
-export default Blog
+export function getStaticProps() {
+  const cmsPosts = postsFromCMS.published.map(post => {
+    const { data } = matter(post);
+    return data;
+  });
 
-/**
- * Need to get the posts from the
- * fs and our CMS
- */
+  const postsPath = path.join(process.cwd(), 'posts');
+  const fileNames = fs.readdirSync(postsPath);
+  const filePosts = fileNames.map(name => {
+     const fullPath = path.join(process.cwd(), 'posts', name);
+     const file = fs.readFileSync(fullPath, 'utf-8');
+     const { data } = matter(file);
+     return data;
+  });
+
+  const posts = [...cmsPosts, ...filePosts];
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Blog
